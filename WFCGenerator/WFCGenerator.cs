b2639cff3d655bd86cf728a9664212f761dad3ac
Sam Vanden Bosch <sam.vanden.bosch@outlook.com>
2026-05-06 14:17:59 +0200

    Added settings and constructor for WFC generator

---
+++ WFCGenerator/WFCGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace WFCGenerator;
+
+public class WFCGenerator
+{
+    private readonly WFCSettings _settings;
+
+    public WFCGenerator(WFCSettings settings)
+        => _settings = settings;
+}
---
+++ WFCGenerator/WFCSettings.cs
@@ -0,0 +1,40 @@
+﻿namespace WFCGenerator;
+
+public record struct WFCSettings
+{
+    public WFCSettings()
+    {
+    }
+
+    /// <summary>
+    /// The example the WFC will base itself on
+    /// </summary>
+    public required int[] Example
+    {
+        get; init;
+    }
+
+    public required int Width
+    {
+        get; init;
+    }
+
+    // the WFC algoritms looks for patterns based on a sample size. The sample will be SampleWidth x SampleHeight in size
+    public required int SampleWidth
+    {
+        get; init;
+    }
+
+    public required int SampleHeight
+    {
+        get; init;
+    }
+
+    /// <summary>
+    /// The seed that is used for generating the final image, when null the generator will use a random seed.
+    /// </summary>
+    public int? Seed
+    {
+        get; set;
+    } = null;
+};
