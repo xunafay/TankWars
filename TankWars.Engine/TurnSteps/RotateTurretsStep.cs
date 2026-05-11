f1b727479a1232c03bf9f2027bc33c71f7cb7e97
Joren Schelkens <joren.schelkens@gmail.com>
2026-05-11 13:36:20 +0000

    Basic implementation for rotate turret

--- TankWars.Engine/TurnSteps/RotateTurretsStep.cs
+++ TankWars.Engine/TurnSteps/RotateTurretsStep.cs
@@ -4,5 +4,11 @@ internal sealed class RotateTurretsStep : ITurnStep
 {
     public void Execute(Game game)
     {
+        foreach (var bot in game.Bots)
+        {
+            var orientation = bot.Implementation.GetTurretRotation();
+            var tank = game.GetTankForBot(bot);
+            tank.Turret.Rotate(orientation);
+        }
     }
 }
